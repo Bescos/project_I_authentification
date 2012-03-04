b7b661f6459a57a2b5930159eedecdcbc900f225
Tommy Bescos <tommy.bescos@etu.enseeiht.fr>
2012-03-04 23:08:55 +0100

    Ajout de la redirection dun nouvel utilisateur vers le formulaire dauthentification

--- sinatra_auth.rb
+++ sinatra_auth.rb
@@ -26,4 +26,21 @@ get '/' do
   end
 end
 
+#La page protégée de l'application est affichée que si l'utilisateur est enregistré et logué 
+get '/appli_cliente1/protected' do
+  if current_user
+    erb :protected, :locals => {:user => current_user}
+  else
+    #redirection vers la page de login avec sauvegarde de l'application demandée dans origine
+    redirect '/sauth/appli_cliente1/sessions/new?origine=/protected'
+  end
+end
+
+#Charge le template erb pour une nouvelle connexion
+get '/sauth/appli_cliente1/sessions/new' do
+  erb :"sessions/new"
+end
+
+
+
 
