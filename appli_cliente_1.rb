991cf9e5524f6d4da7c55d72dd7e5f29a25cc883
Tommy Bescos <tommy.bescos@etu.enseeiht.fr>
2012-03-05 16:04:54 +0100

    Mise a jour du squelette du projet en separant les applications du Sauth

---
+++ appli_cliente_1.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+$: << File.join(File.dirname(__FILE__),"","middleware")
+require 'my_middleware'
+
+use RackCookieSession
+use RackSession
+
+helpers do 
+  def current_user
+    session["current_user"]
+  end
+
+  def disconnect
+    session["current_user"] = nil
+  end
+end
+
+
+#La page protégée de l'application est affichée que si l'utilisateur est enregistré et logué 
+get '/appli_cliente1/protected' do
+  if current_user
+    erb :protected, :locals => {:user => current_user}
+  else
+    #redirection vers la page de login avec sauvegarde de l'application demandée dans origine
+    redirect '/sauth/appli_cliente1/sessions/new?origine=/protected'
+  end
+end
+
+
