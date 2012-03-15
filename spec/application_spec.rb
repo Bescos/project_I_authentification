d0b0290805b0a9e144ba113eb2115c3f4c8a80f3
Tommy Bescos <tommy.bescos@etu.enseeiht.fr>
2012-03-15 19:12:27 +0100

    Refactorisation du code de application_spec

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -5,50 +5,49 @@ describe Application do
  context "Test with 2 applications" do
   before(:each) do
     @a1 = Application.new 
-    @a2 = Application.new 
-    @a3 = Application.new
-  end
-  it "sould not be valid without a name, an url or an user_id" do
-    subject.should_not be_valid
-    @a1.url = 'http://testPresence'
-    @a1.user_id = '1'
-    @a1.should_not be_valid
-    @a2.name = 'testPresence'
-    @a2.user_id = '1'
-    @a2.should_not be_valid
-    @a3.url = 'http://testPresence'
-    @a3.name = 'testPresence'
-    @a3.should_not be_valid
-  end
-  context "Uniqueness of url and name" do
-   before (:each) do
-    @a1.name = 'testUniqueness'
-    @a1.url = 'http://testUniqueness'
+    @a1.name = 'testApplication'
+    @a1.url = 'http://testApplication'
     @a1.user_id = '1'
     @a1.should be_valid
     @a1.save
-   end
-   it "should not be valid if name already exists" do
-   @a2.name = 'testUniqueness'  
-   @a2.url = 'http://testUniqueness2' 
-   @a2.user_id = '2'
-   @a2.should_not be_valid
-   Application.all.each{|a| a.destroy}
-   end
-
-   it "should not be valid if url already exists" do
-    @a2.name = 'testUniqueness2'  
-    @a2.url = 'http://testUniqueness' 
-    @a2.user_id = '2'
-    @a2.should_not be_valid
-    Application.all.each{|a| a.destroy}
-   end
   end
-  it "should not be valid with a non numeric user_id" do
-   @a1.name = 'testUniqueness'
-   @a1.url = 'http://testUniqueness'
+  after(:each) do
+    Application.all.each{|a| a.destroy}
+	end
+
+  it "sould not be valid without a name" do
+    @a1.name = nil
+    @a1.should_not be_valid
+  end
+  it "sould not be valid without an url" do
+    @a1.url = nil
+    @a1.should_not be_valid
+  end
+  it "sould not be valid without an user_id" do
+    @a1.user_id = nil
+    @a1.should_not be_valid
+  end
+   it "should not be valid with a non numeric user_id" do
    @a1.user_id = 'test'
    @a1.should_not be_valid
+  end
+
+  context "Uniqueness of url and name" do
+   before (:each) do
+		@a2=Application.new
+    @a2.name = 'testApplication2' 
+    @a2.url = 'http://testApplication2' 
+    @a2.user_id = '2'
+   end
+
+   it "should not be valid if name already exists" do
+   @a2.name = 'testApplication'  
+   @a2.should_not be_valid
+   end
+   it "should not be valid if url already exists" do
+    @a2.url = 'http://testApplication' 
+    @a2.should_not be_valid
+   end
   end
  end
  
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -17,29 +17,25 @@ describe User do
 		 it "should be valid with a login and a password" do
 			@u1.should be_valid
 		 end
-
 		 it "sould not be valid without a login or a password" do
-			 subject.should_not be_valid
 			 @u1.login = ""
 			 @u1.should_not be_valid
-		 	 @u1.login = "TestValid"
+			end
+     it "sould not be valid without a password" do
 			 @u1.password = ""
 			 @u1.should_not be_valid
 			end
-
 		 it "should not be valid when login already exists" do
 			 u2 = User.new
 			 u2.login = "TestValid" 
 			 u2.password = "TestUniqueness"
 			 u2.should_not be_valid
 		 end
-
 		 it "should encode the password with sha1" do
 			 User.should_receive(:encrypt_password).with('TestValid').and_return("\"978d8399a65d10b8b3f58338f68ee72192428bd1\"") #Return Sha1 Code of TestPassword
        @u1.password = "TestValid"
 			 @u1.password.should == "\"978d8399a65d10b8b3f58338f68ee72192428bd1\""
 		 end
-
 		 it "should not encode the password because its empty" do
 			 @u1.password=''
 			 @u1.password.should be_nil
